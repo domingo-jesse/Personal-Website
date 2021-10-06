d0de908b053b4b727b202e185499f95ebc7f1212
domingo-jesse <domingo.jesse@gmail.com>
2021-10-06 02:40:06 -0700

    deleted duplicate code

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,15 +12,3 @@ const IndexPage = () => {
 }
 // Step 3: Export your component
 export default IndexPage
-=======
-  return (
-    <main>
-      <title>Home Page</title>
-      <h1>Welcome to my Gatsby site!</h1>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-    </main>
-  )
-}
-// Step 3: Export your component
-export default IndexPage
->>>>>>> 95bd33d4c0ee8d5bd09e10c19f0228bc90fd7ab0
