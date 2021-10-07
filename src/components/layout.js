c2249fe9250326482788ea938b05276a2f207067
domingo-jesse <domingo.jesse@gmail.com>
2021-10-07 02:01:05 -0700

    testing new header

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,53 +1,41 @@
-import * as React from 'react'
-import { Link, useStaticQuery, graphql } from 'gatsby'
-import {
-    container,
-    heading,
-    navLinks,
-    navLinkItem,
-    navLinkText,
-    siteTitle,
-} from './layout.module.css'
-
-const Layout = ({ pageTitle, children }) => {
-    const data = useStaticQuery(graphql`
-    query {
-      site {
-        siteMetadata {
-          title
+const Layout = ({ children }) => (
+    <StaticQuery
+        query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
++            menuLinks {
++              name
++              link
++            }
+          }
         }
       }
-    }
-  `)
-    return (
-        <div className={container}>
-            <title>{pageTitle} | {data.site.siteMetadata.title}</title>
-            <header className={siteTitle}>{data.site.siteMetadata.title}</header>
-            <nav>
-                <ul className={navLinks}>
-                    <li className={navLinkItem}>
-                        <Link to="/" className={navLinkText}>
-                            Home
-            </Link>
-                    </li>
-                    <li className={navLinkItem}>
-                        <Link to="/about" className={navLinkText}>
-                            About
-            </Link>
-                    </li>
-                    <li className={navLinkItem}>
-                        <Link to="/blog" className={navLinkText}>
-                            Blog
-            </Link>
-                    </li>
-                </ul>
-            </nav>
-            <main>
-                <h1 className={heading}>{pageTitle}</h1>
-                {children}
-            </main>
-        </div>
-    )
-}
-
-export default Layout
+    `}
+        render={data => (
+            <React.Fragment>
+                <Helmet
+                    title={'title'}
+                    meta={[
+                        { name: 'description', content: 'Sample' },
+                        { name: 'keywords', content: 'sample, something' },
+                    ]}
+                >
+                </Helmet>
+-      <Header siteTitle={data.site.siteMetadata.title} />
++      <Header menuLinks={data.site.siteMetadata.menuLinks} siteTitle={data.site.siteMetadata.title} />
+                <div
+                    style={{
+                        margin: '0 auto',
+                        maxWidth: 960,
+                        padding: '0px 1.0875rem 1.45rem',
+                        paddingTop: 0,
+                    }}
+                >
+                    {children}
+                </div>
+            </React.Fragment>
+        )}
+    />
+)
