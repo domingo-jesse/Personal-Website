3eb4dab91837dc5ffa919950b23865d7c616b096
domingo-jesse <domingo.jesse@gmail.com>
2021-10-06 20:59:32 -0700

    title added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "Jesse Domingo Website",
+    title: "Jesse Domingo Personal Website",
   },
     plugins: [
         "gatsby-plugin-image",
