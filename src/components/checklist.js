a720e88986312c65f19a2546f19040b638cf388a
domingo-jesse <domingo.jesse@gmail.com>
2021-10-09 01:51:34 -0700

    added checklist componet

---
+++ src/components/checklist.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const CheckList  = ({ pageTitle, children }) => {
+        return (
+            <div> 
+                <h1>Hello</h1> 
+
+        </div>
+    )
+}
+
+export default CheckList
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import CheckList from '../components/checklist'
 const AboutPage = () => {
     return (
         <Layout pageTitle="About Me">
             <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+            <CheckList /> 
         </Layout>
     )
 }
